bb16c40933034f5dfc9c7c8c6e703f6584df584d
Richard <richardecos@gmail.com>
2016-09-14 23:12:49 -0400

    #3 Add gitigonre and eslintrc files

--- server/src/controllers/client.js
+++ server/src/controllers/client.js
@@ -1,33 +1,32 @@
 // var People = require('./people.js');
 
-function People(firstName, lastName) {
-  this.firstName = firstName;
-  this.lastName = lastName;
+function People (firstName, lastName) {
+  this.firstName = firstName
+  this.lastName = lastName
 }
 
-People.prototype.getPeople = function() {
-  return this.firstName+ ''+lastName;
-};
-
-function Client(firstName, lastName, identification, category) {
-  People.call(this, firstName, lastName);
-  this.identification = identification;
-  this.category = category;
-
-  var client = {
-    firstName: this.firstName,
-    lastName: this.lastName,
-    identification: this.identification,
-    category: this.category
-  };
+var client = {
+  firstName: this.firstName,
+  lastName: this.lastName,
+  identification: this.identification,
+  category: this.category
 }
 
-Client.prototype = new People();
+People.prototype.getPeople = function () {
+  return this.firstName + '' + this.lastName
+}
 
-Client.prototype.getClient = function() {
-  return client;
-};
+function Client (firstName, lastName, identification, category) {
+  People.call(this, firstName, lastName)
+  this.identification = identification
+  this.category = category
+}
 
-var a = new Client("Richard","Ecos", 5287787, 'Admin');
-console.log(People.getPeople);
+Client.prototype = new People()
 
+Client.prototype.getClient = function () {
+  return client
+}
+
+// var aclient = new Client('Richard', 'Ecos', 5287787, 'Admin')
+console.log(People.getPeople)
